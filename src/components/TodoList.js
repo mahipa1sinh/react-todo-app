0eae524131db3b765880d0358803ffac2e61da25
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2023-02-06 16:44:44 +0530

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -50,7 +50,7 @@ function TodoList() {
 
   return (
     <>
-      <h1>What's the Plan for Today?</h1>
+      <h1>✅Plan your day here.✅</h1>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
