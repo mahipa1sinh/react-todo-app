929faab5ff70e723c86af8e17b5d577b437a6276
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2023-02-07 10:59:24 +0530

    working

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,9 +1,29 @@
 import React from "react";
 import "./ListItems.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  cursor: pointer;
+  &:hover {
+    background: palevioletred;
+    color: white;
+  }
+`;
 
 function ListItems(props) {
   const items = props.items;
+  //   const [complete, setComplete] = useState(false);
+
+  //   const handleComplete = (id) => {
+  //     setComplete(!complete);
+  //   };
+
   const listItems = items.map((item) => {
     return (
       <div className="list" key={item.key}>
@@ -24,6 +44,15 @@ function ListItems(props) {
               }}
               icon="trash"
             />
+            {/* <Button
+              id={item.key}
+              className="listButton"
+              onClick={() => {
+                handleComplete(item.key);
+              }}
+            >
+              Done
+            </Button> */}
           </span>
         </p>
       </div>
